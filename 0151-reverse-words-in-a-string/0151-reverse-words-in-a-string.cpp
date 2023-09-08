89c4410f0b9adc9002c8e2a52b981758240b95ea
Manasvi <103330308+manasvi83@users.noreply.github.com>
2023-09-08 14:28:20 +0530

    Time: 15 ms (11.29%), Space: 20.8 MB (21.07%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string ans="";
+        string temp= "";
+        int n=s.size();
+        for(int i=0; i<n ; i++)
+        {
+            if(s[i]==' ' && temp=="")
+            {
+                continue;
+            }
+            else if(s[i]==' ' && temp!= "")
+            {
+                cout<<temp<<endl;
+                if(ans=="") ans=temp+ans;
+                else ans= temp + " "+ans;
+                temp="";
+            }            
+            else if(s[i]!=' ')  
+            {
+                temp += s[i];
+            }
+        }        
+        if(temp=="") return ans;
+        else if(temp!=""&& ans=="") return temp+ans;
+        return temp + " " +ans;
+    
+    }
+};
