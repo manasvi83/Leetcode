fcabbaf5562f6408ea21064d880f7d3d19a21aac
Manasvi <103330308+manasvi83@users.noreply.github.com>
2024-01-08 23:56:24 +0530

    Time: 165 ms (79.70%), Space: 110.9 MB (96.17%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -13,8 +13,13 @@ public:
     bool isPalindrome(ListNode* head) {
         ListNode *slow = head, *fast = head, *prev = NULL, *tmp;
 	    while(fast && fast -> next) 
-		    fast = fast -> next -> next,
-		    tmp = slow -> next, slow -> next = prev, prev = slow, slow = tmp;        
+        {
+            fast = fast -> next -> next;
+		    tmp = slow -> next;
+            slow -> next = prev;
+            prev = slow;
+            slow = tmp;  
+        }
 	        slow = (fast ? slow -> next : slow); 
 	    while(slow) 
 		if(slow -> val != prev -> val) return false;
