db5e8101df392070f2b92f0fb9d978c04990012c
Manasvi <103330308+manasvi83@users.noreply.github.com>
2024-02-20 01:43:13 +0530

    Time: 5 ms (20.14%), Space: 7.2 MB (40.89%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    int isPowerOfTwo(int n) {
-        if(n<=0) return false;
-        else if((n & (n-1))==0)  return true;
-        return false;
+    bool isPowerOfTwo(int n) {
+        if(n==0) return false;
+        while(n%2==0) n/=2;
+        return n==1;
     }
 };
