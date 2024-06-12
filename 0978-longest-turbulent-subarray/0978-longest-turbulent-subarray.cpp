1c1172926c4518c3c1fe4dc898860811de1cad49
Manasvi <103330308+manasvi83@users.noreply.github.com>
2024-06-12 14:37:19 +0530

    Time: 58 ms (32.34%), Space: 42.8 MB (27.20%) - LeetHub

--- 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.cpp
+++ 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.cpp
@@ -4,7 +4,7 @@ public:
         int ans = 1;
         int smaller = 1;
         int larger = 1;
-        for (int i = 0; i < arr.size() - 1; i++) {
+        for (int i = 0; i < arr.size() - 1; ++i) {
             smaller = arr[i] < arr[i + 1] ? smaller + 1 : 1;
             larger = arr[i] > arr[i + 1] ? larger + 1 : 1;
             
